7b985f7d92efb41f8470fcafc286ef8a75c8bed2
soniatocode <sonakshi_singh@hotmail.com>
2019-12-03 00:15:34 -0800

    made some changes

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@main.route('/rmain')
+def rmain():
+    return render_template("rmain.html")
 
     
 if __name__ == "__main__":
