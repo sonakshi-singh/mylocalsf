daaeadcc3b6ee6839c3299eb1473cd1617f669d9
soniatocode <sonakshi_singh@hotmail.com>
2019-12-02 16:45:10 -0800

    made some changes

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template      
+from flask import Flask, render_template  
+from .extensions import db, login_manager    
+def create_app(config_file='settings.py'):
+	app = Flask(__name__)
 
-app = Flask(__name__)
+	app.config.from_pyfile(config_file )
+
+	db.init_app(app)
+
+	login_manager.init_app(app)
+
+	#login_manager.login_view=''
+	#login_manager.user_loader
+	#def load_user(user_id):
+		#return User.query.get(user_id)
+
+
+
+	return app 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/")
+def rmain():
+    return render_template("rmain.html")
     
 
     
