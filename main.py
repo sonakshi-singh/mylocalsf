148ddd6b15c9fc27b497dbfdc225d954a87117e5
soniatocode <sonakshi_singh@hotmail.com>
2019-12-03 00:27:45 -0800

    made some changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app import app
+
 from flask import Flask, render_template  
 
 app=Flask(__name__)
@@ -8,9 +8,7 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-
-
-    
+  
 if __name__ == "__main__":
     app.run()
   
