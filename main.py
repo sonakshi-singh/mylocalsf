114464e3f062fd68485861a3dd5c2e45bd32e8cd
soniatocode <sonakshi_singh@hotmail.com>
2019-12-02 17:55:05 -0800

    made some changes

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def create_app(config_file='settings.py'):
 def home():
     return render_template("index.html")
 
-@app.route("/rmain.html")
+@app.route("/")
 def rmain():
     return render_template("rmain.html")
     
